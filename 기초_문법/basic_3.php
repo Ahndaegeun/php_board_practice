bf0eff207aebae41e25eee21e53ef4ed02841e59
ahndaegeun <adg0807@naver.com>
2022-03-17 15:49:38 +0900

    php 조건문, 반복문

---
+++ 기초_문법/basic_3.php
@@ -0,0 +1,40 @@
+<?php
+// 조건문
+if(true) {
+
+} else if( 1 > 1) {
+
+} else {
+
+}
+
+switch('a') {
+  case '1':
+    break;
+  case 'a':
+    break;
+  default:
+    break;
+}
+
+// 반복문
+while(true) {
+
+  break;
+}
+
+do {
+
+} while(false);
+
+for($i = 0; $i < 10; $i++) {
+
+}
+
+$arr = array('a' => 1, 'b' => 2);
+foreach($arr as $i) {
+  echo $i;
+}
+
+
+?>
